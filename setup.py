cfdae491c2fa9d93396e5e6d54f2fc46e7f1d5a5
Oscar Gonzalez <oscgonfer@gmail.com>
2020-03-03 18:03:19 +0100

    Setup fix and .dotfile

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='SmartCitizen Sensor Analysis Framework',
+    author='oscgonfer',
+    license='GNU-GPL3.0',
+)
