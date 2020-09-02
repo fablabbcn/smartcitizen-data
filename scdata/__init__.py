64d85869e37babbffc989b2631d0c7b3984707b6
Oscar Gonzalez <oscgonfer@gmail.com>
2020-09-02 17:50:20 +0200

    Loads test, device and config on init

--- scdata/__init__.py
+++ scdata/__init__.py
@@ -1,5 +1,7 @@
-# from .test import Test
-# from .device import Device
+from .test import Test
+from .device import Device
+from ._config import config
+
 # from .batch import Batch
 
 __version__ = '0.1.0'
