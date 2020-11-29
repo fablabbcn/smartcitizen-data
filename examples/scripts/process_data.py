1195e9b8e3a6ccbd911aa6ebb7ca7c49c6db900f
Oscar Gonzalez <oscgonfer@gmail.com>
2020-11-29 18:11:54 +0100

    Process data example fix

--- examples/scripts/process_data.py
+++ examples/scripts/process_data.py
@@ -2,11 +2,11 @@
 
 import scdata as sc
 
-device = sc.Device(blueprint = 'sck_21_station_box', descriptor = {'source': 'api', 'id': '10751'})
+device = sc.Device(blueprint = 'sc_21_station_box', descriptor = {'source': 'api', 'id': '10751'})
 
 device.api_device.post_info = dict()
 device.api_device.post_info['hardware_id'] = "SCTEST"
 
 device.__fill_metrics__()
-
+device.load()
 device.process()
