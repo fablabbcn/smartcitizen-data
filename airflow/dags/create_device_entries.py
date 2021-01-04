db90c64a288da035378385caf4359a2c4a96e03b
Oscar Gonzalez <oscgonfer@gmail.com>
2021-01-04 21:21:27 +0100

    Dag with postgres for device entries

--- airflow/dags/create_device_entries.py
+++ airflow/dags/create_device_entries.py
@@ -12,34 +12,34 @@ config._out_level = 'DEBUG'
 
 from airflow import DAG
 from airflow.models import Variable
-from airflow.hooks.sqlite_hook import SqliteHook
-from airflow.operators.sqlite_operator import SqliteOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 import urllib.request
 
-DEVICES_URL = Variable.get("devices_url") #https://raw.githubusercontent.com/fablabbcn/smartcitizen-data/master/airflow/devices/devices.txt
-DEVICES_TABLE = Variable.get("post_devices_table") #devices
+DEVICES_TABLE = Variable.get('post_devices_table') #post_devices_table
 
 default_args = {
-                "start_date": days_ago(1),
-                "retries": 2,
-                "retry_delay": timedelta(minutes=1),
+                'start_date': days_ago(1),
+                'retries': 2,
+                'retry_delay': timedelta(minutes=1),
                 'provide_context': True
                 }
 
 dag = DAG(
             dag_id='devices_update',
-            description = "Create and/or update tables to process devices",
+            description = 'Create and/or update tables to process devices',
             default_args = default_args,
-            schedule_interval = "@daily"
+            schedule_interval = '@daily'
           )
 
 
 def get_devices_to_process(**kwargs):
+    df = ScApiDevice.search_query(key = 'postprocessing_info', value = 'not_null', full = True)
     devices_raw = dict()
-    for line in urllib.request.urlopen(kwargs['url']):
-        devices_raw[line.decode('utf-8')] = {'last_reading': None, 'latest_postprocessing': None}
+    for item in df.index:
+        devices_raw[item] = df.loc[item, 'postprocessing_info']
     return devices_raw
 
 def get_devices_dates(**kwargs):
@@ -52,29 +52,24 @@ def get_devices_dates(**kwargs):
         location = apidevice.get_device_location()
         last_reading = apidevice.get_device_last_reading()
         
-        try:
-            latest_postprocessing = apidevice.get_postprocessing_info()['latest_postprocessing']
-        except:
-            latest_postprocessing = None
-            pass
-
-        devices[device]["last_reading"] = localise_date(last_reading, location).__str__()
-
-        devices[device]["latest_postprocessing"] = localise_date(latest_postprocessing, location).__str__()
+        devices[device]['last_reading'] = localise_date(last_reading, location).__str__()
 
     return devices
 
 def fill_devices_table(**kwargs):
     ti = kwargs['ti']
     devices = ti.xcom_pull(task_ids='get_devices_dates')
-    conn_host = SqliteHook(sqlite_conn_id='sqlite_devices').get_conn()
+    conn_host = PostgresHook(postgres_conn_id='postgres_default').get_conn()
     
     for device in devices:
         sql_insert = f"""INSERT OR REPLACE INTO {DEVICES_TABLE} 
-                     (device, last_reading, latest_postprocessing)
+                     (device, last_reading, latest_postprocessing, updated_at, blueprint_url, hardware_url)
                      VALUES ({device},
                              '{devices[device]["last_reading"]}',
-                             '{devices[device]["latest_postprocessing"]}'
+                             '{devices[device]["latest_postprocessing"]}',
+                             '{devices[device]["updated_at"]}',
+                             '{devices[device]["blueprint_url"]}',
+                             '{devices[device]["hardware_url"]}'
                             )
                      ;"""
         
@@ -95,16 +90,19 @@ with dag:
         python_callable = get_devices_dates    
     )
 
-    create_devices_table = SqliteOperator(
+    create_devices_table = PostgresOperator(
         task_id = "create_devices_table",
         sql=f"""
                 CREATE TABLE IF NOT EXISTS {DEVICES_TABLE}(
                 device INT,
                 last_reading DATE,
-                latest_postprocessing DATE
+                latest_postprocessing DATE,
+                updated_at DATE,
+                blueprint_url TEXT,
+                hardware_url TEXT
                 );
             """,
-        sqlite_conn_id = "sqlite_devices"
+        postgres_conn_id = "postgres_default"
         )
 
     fill_devices_table = PythonOperator(
@@ -118,14 +116,14 @@ with dag:
     # if a prediction with a same device already exists,
     # it updates that particular row with the new values,
     # otherwise it inserts a new record.
-    create_device_index = SqliteOperator(
-        task_id="create_device_index",
+    create_device_index = PostgresOperator(
+        task_id='create_device_index',
         sql=f"""
                 CREATE UNIQUE INDEX IF NOT EXISTS idx_device
                 ON {DEVICES_TABLE} (device)
                 ;
             """,
-        sqlite_conn_id="sqlite_devices"
+        postgres_conn_id='postgres_default'
         )
 
     create_devices_table >> create_device_index
