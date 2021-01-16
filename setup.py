537dcf0b908998becf9bcda6b4e2ffaa1d3a3a39
Oscar Gonzalez <oscgonfer@gmail.com>
2021-01-16 21:19:47 +0100

    Bump to version 0.2.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PROJECT_URLS = {
 
 setup(
     name='scdata',
-    version= '0.2.2',
+    version= '0.2.3',
     description='Analysis of sensors and time series data',
     author='oscgonfer',
     license='GNU-GPL3.0',
