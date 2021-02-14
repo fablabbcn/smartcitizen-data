21b73db8a0da2af4f512eeb7b452f9b9be0490c2
Oscar Gonzalez <oscgonfer@gmail.com>
2021-02-14 18:13:23 +0100

    Cosmetic cleanup and avoid typeerror on blueprint loads

--- scdata/utils/meta.py
+++ scdata/utils/meta.py
@@ -44,15 +44,15 @@ def get_paths():
     _paths['data'] = _scddir
 
     # Auxiliary folders
-    
+
     # - Processed data
     _paths['processed'] = join(_paths['data'], 'processed')
     makedirs(_paths['processed'], exist_ok=True)
-    
+
     # - Internal data: blueprints and calibrations
     _paths['interim'] = join(_paths['data'], 'interim')
     makedirs(_paths['interim'], exist_ok=True)
-    
+
     # Check for blueprints and calibrations
     # Find the path to the interim folder
     _dir = dirname(__file__)
@@ -61,11 +61,11 @@ def get_paths():
     # - Models and local tests
     _paths['models'] = join(_paths['data'], 'models')
     makedirs(_paths['models'], exist_ok=True)
-    
+
     # - Exports
     _paths['export'] = join(_paths['data'], 'export')
     makedirs(_paths['export'], exist_ok=True)
-    
+
     # - Raw
     _paths['raw'] = join(_paths['data'], 'raw')
     makedirs(_paths['raw'], exist_ok=True)
@@ -77,11 +77,11 @@ def get_paths():
     # - Reports
     _paths['reports'] = join(_paths['data'], 'reports')
     makedirs(_paths['reports'], exist_ok=True)
-    
+
     # - Uploads
     _paths['uploads'] = join(_paths['data'], 'uploads')
     makedirs(_paths['uploads'], exist_ok=True)
-    
+
     # Check for uploads
     _example_uploads = ['example_upload_1.json', 'example_zenodo_upload.yaml']
     _udir = join(_dir, 'uploads')
@@ -89,7 +89,7 @@ def get_paths():
         s = join(_udir, item)
         d = join(_paths['uploads'], item)
         if not exists(d): copyfile(s, d)
-    
+
     # Inventory (normally not used by user)
     _paths['inventory'] = ''
 
@@ -104,7 +104,7 @@ def load_env(env_file):
                 # Load to local environ
                 key, value = line.strip().split('=', 1)
                 environ[key] = value
-    
+
     except FileNotFoundError:
         print('.env file not found')
         return False
@@ -115,12 +115,13 @@ def load_blueprints(urls):
 
     blueprints = dict()
     for url in urls:
-        _nblueprint = basename(urlparse(url).path).split('.')[0]
+        if url is None: continue
+        _nblueprint = basename(urlparse(str(url)).path).split('.')[0]
         _blueprint = get_json_from_url(url)
 
         if _nblueprint not in blueprints:
             blueprints[_nblueprint] = _blueprint
-            
+
     return blueprints
 
 def get_current_blueprints():
