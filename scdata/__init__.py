d8754ebd3811814df8f60c0cebcf2dd8e2225997
Oscar Gonzalez <oscgonfer@gmail.com>
2021-03-07 16:31:18 +0100

    Bump to 0.3.4

--- scdata/__init__.py
+++ scdata/__init__.py
@@ -1,4 +1,4 @@
 from .test import Test
 from .device import Device
 
-__version__ = '0.3.2'
+__version__ = '0.3.4'
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ PROJECT_URLS = {
 
 setup(
     name='scdata',
-    version= '0.3.2',
+    version= '0.3.4',
     description='Analysis of sensors and time series data',
     author='oscgonfer',
     license='GNU-GPL3.0',
