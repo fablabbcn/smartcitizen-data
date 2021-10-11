f14d234f018b92c0cbfc4b3996c2c0e94c0494cf
Oscar Gonzalez <oscgonfer@gmail.com>
2021-10-11 15:28:27 +0200

    Add max forward retries to device process task

--- tasks/dprocess.py
+++ tasks/dprocess.py
@@ -21,11 +21,11 @@ def dprocess(device, dryrun = False):
             # Process it
             d.process()
             # Post results if not dry run
-            d.post_metrics(dry_run=dry_run)
+            d.post_metrics(dry_run=dry_run, max_retries = config._max_forward_retries)
             # Forward it if requested
             if d.forwarding_request is not None:
                 std_out(f'[CHUPIFLOW] Forwarding {device}')
-                d.forward(dry_run=dry_run)
+                d.forward(dry_run=dry_run, max_retries = config._max_forward_retries)
             d.update_postprocessing(dry_run=dry_run)
     else:
         std_out(f'[CHUPIFLOW] Device {device} not valid', 'ERROR')
