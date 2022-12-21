1f8e1a3aabf1f88552c3eba14ba671bcb238680f
Oscar Gonzalez <oscgonfer@gmail.com>
2022-12-21 15:00:40 +0100

    Eliminate dependencies for reports, and have them as external requirements

--- scdata/utils/__init__.py
+++ scdata/utils/__init__.py
@@ -5,7 +5,6 @@ from .dictmerge import dict_fmerge
 from .lazy import LazyCallable
 from .logs import get_tests_log
 from .meta import get_current_blueprints, load_blueprints, get_json_from_url
-from .report import include_footer
 from .stats import spearman, get_metrics
 from .cleaning import clean
 from .location import get_elevation
@@ -13,3 +12,4 @@ from .url_check import url_checker
 from .headers import process_headers
 # from .other.manage_post_info import create_post_info
 # from .zenodo import zenodo_upload
+# from .report import include_footer
