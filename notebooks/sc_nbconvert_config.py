82085f00d019b66e2bdb5932e629ed5a4046b290
oscgonfer <oscgonfer@gmail.com>
2024-07-19 16:13:56 +0200

    Remove notebooks and analysis. Move to jbook

--- notebooks/sc_nbconvert_config.py
+++
@@ -1,5 +0,0 @@
-c = get_config()
-
-#Export all the notebooks in the current directory to the sphinx_howto format.
-c.Exporter.preprocessors = ['sc_preprocessor.SCPreprocessor']
-# c.NbConvertApp.writer_class = 'notebook_copy_writer.NotebookCopyWriter'
