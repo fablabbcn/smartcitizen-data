1080564fe4884b95baa4f254081a5ade9d877d69
oscgonfer <oscgonfer@gmail.com>
2024-11-27 14:03:26 +0100

    Bump to version 1.2.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(
     name='scdata',
-    version='1.2.1',
+    version='1.2.2',
     description='Analysis of sensors and time series data',
     author='oscgonfer',
     license='GNU-GPL3.0',
