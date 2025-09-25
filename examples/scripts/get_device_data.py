6b504dd37abb8330fb25f9d6e1b07f3e47235063
oscgonfer <oscgonfer@gmail.com>
2025-09-25 12:05:03 +0200

    Fix device data example

--- examples/scripts/get_device_data.py
+++ examples/scripts/get_device_data.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python
 
-from smartcitizen_connector import SCDevice
+import scdata as sc
 from scdata._config import config
+import asyncio
 
 # Set verbose level
 config.log_level = 'DEBUG'
 
 # Device id needs to be as str
-device = SCDevice(10972)
+device = sc.Device(blueprint='sc_air',
+                               params=sc.APIParams(id=16784))
 device.options.min_date = None #Don't trim min_date
 device.options.max_date = None #Don't trim max_date
+device.options.frequency = '1Min' # Use this to change the sample frequency for the request
+print (device.json)
 
 # Load
-await data = device.g(min_date = None, max_date = None, frequency = '1Min', clean_na = None);
+asyncio.run(device.load())
 
-print (data)
+print (device.json)
+print (device.data)
