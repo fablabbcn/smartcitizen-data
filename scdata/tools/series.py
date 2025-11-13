05cfd95e6174366cf7f03ec10fe7693d04bfb15b
Daniel Mateos <daniel@mateos.io>
2025-11-13 10:48:30 +0100

    Utility functions for health metrics
    Utility functions that needn't be inside Device:
    - infer_sampling_rate
    - mode_count

---
+++ scdata/tools/series.py
@@ -0,0 +1,32 @@
+from scdata.tools.custom_logger import logger
+
+from pandas import Series, Timedelta
+import numpy as np
+
+def infer_sampling_rate(series: Series) -> int | None:
+    '''Infer the sampling rate of the given timeseries, rounded to the 
+    closest minute.
+    '''
+    
+    time_differences = series.index.diff().value_counts()
+    most_common = time_differences.index[0]
+    
+    minutes = most_common / Timedelta("1min")
+    integer_minutes = round(minutes)
+    
+    if abs(integer_minutes - minutes) > 0.05:
+        logger.warning('Rounded a time difference with more than 5% error')
+        return None
+        
+    return integer_minutes
+
+
+def mode_count(series: Series, ignore_zeroes=True) -> int:
+    '''Count the number of times the most common value appears in series,
+    ignoring zeroes.'''
+    
+    if ignore_zeroes:
+        # Replace zeroes with random so that they don't impact value count
+        series = series.where(series!=0.0, np.random.random(size=series.size))  
+        
+    return series.value_counts().iloc[0]
